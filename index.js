99bab0912ab8b424038a5063bdc3ea22aa0141bb
Kebron Mehari <kebronmehari400@gmail.com>
2022-04-08 21:34:20 -0600

    Completed assignment

---
+++ index.js
@@ -0,0 +1,16 @@
+function divide() {
+    return 2000 / 100;
+  }
+  
+  console.log(divide( 2000 / 100));
+
+  square = () => {
+    let x = 2;
+   return x*x
+  }
+
+  function add() {
+  let a = 3;
+   let b = 4;
+   return a + b ;
+}
